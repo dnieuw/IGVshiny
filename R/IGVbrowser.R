acf1bac1ff34caac925e683f8037216917264cfe
dnieuw <davidnieuwenhuijse@gmail.com>
2016-11-16 15:53:00 +0100

    Adjusting js and R to allow custom track loading from R

--- R/IGVbrowser.R
+++ R/IGVbrowser.R
@@ -1,7 +1,9 @@
-#' <Add Title>
+#' Shiny widget implementation of igv.js
 #'
-#' <Add Description>
-#'
+#' This shiny widget implements the javascript based Integrative Genomics Viewer made by the Broad Institute.
+#' To follow the developments of igv.js please to https://github.com/igvteam/igv.js.
+#' I do not claim any ownership of igv.js, I just want to be able to implement it in a shiny dashboard app.
+#' 
 #' @import htmlwidgets
 #'
 #' @export
@@ -21,6 +23,57 @@ IGVbrowser <- function(message, width = NULL, height = NULL, elementId = NULL) {
     package = 'IGVshiny',
     elementId = elementId
   )
+}
+
+#' IGV Annotation track factory IGVbrowser
+#'
+#' Outputs a dataframe which can be transformed correctly to json format and serve as the options of a igv.js annotation track.
+#'
+#' @param type Track type
+#' @param sourceType Type of data source. Valid values are "file", "gcs" for Google Cloud Storage, and "ga4gh" for the Global Alliance API
+#' @param format File format
+#' @param name Display name (label). Required 
+#' @param url URL to the track data resource, such as a file or webservice. Required 
+#' @param indexURL URL to a file index, such as a BAM .bai, Tabix .tbi, or Tribble .idx file.
+#' @param indexed Flag used to indicate if a file is indexed or not. If indexURL is provided this flag is redundant, its main purpose is to indicate that a file is not indexed.
+#' @param order Integer value specifying relative order of track position on the screen. To pin a track to the bottom use Number.MAX_VALUE. If no order is specified, tracks appear in order of their addition.
+#' @param color CSS color value for track features, e.g. "#ff0000" or "rgb(100,0,100)"
+#' @param height Initial height of track viewport in pixels
+#' @param autoHeight If true, then track height is adjusted dynamically, within the bounds set by minHeight and maxHeight, to accomdodate features in view
+#' @param minHeight Minimum height of track in pixels
+#' @param maxHeight Maximum height of track in pixels
+#' @param visibilityWindow Maximum window size in base pairs for which indexed annotations or variants are displayed
+#' @param displayMode Annotation display mode, one of "COLLAPSED", "EXPANDED", "SQUISHED"
+#' @param expandedRowHeight Height of each row of features in "EXPANDED" mode
+#' @param squishedRowHeight Height of each row of features in "SQUISHED" mode
+#' @param nameField For GFF/GTF file formats. Name of column 9 property to be used for feature label
+#' @param maxRows Maximum number of rows of features to display
+#' @param searchable If true, feature names for this track can be searched for. Use this option with caution, it is memory intensive. This option should will not work with indexed tracks.
+#' 
+#' @name IGVbrowser-shiny
+#'
+#' @export
+IGVannotationTrack <- function(type="annotation",
+                               sourceType="file",
+                               format,
+                               name,
+                               url,
+                               indexURL,
+                               indexed,
+                               order,
+                               color,
+                               height,
+                               autoHeight=TRUE,
+                               minHeight,
+                               maxHeight,
+                               visibilityWindow=30000,
+                               displayMode="COLLAPSED",
+                               expandedRowHeight,
+                               squishedRowHeight,
+                               nameField="Name",
+                               maxRows,
+                               searchable=FALSE){
+  return(data.frame(type=type,sourceType=sourceType,###ETC))
 }
 
 #' Shiny bindings for IGVbrowser
--- inst/htmlwidgets/IGVbrowser.js
+++ inst/htmlwidgets/IGVbrowser.js
@@ -16,30 +16,30 @@ HTMLWidgets.widget({
         bedindex = "https://s3.amazonaws.com/igv.broadinstitute.org/annotations/hg19/genes/refGene.hg19.bed.gz.tbi",
         bam = "https://data.broadinstitute.org/igvdata/BodyMap/hg19/IlluminaHiSeq2000_BodySites/brain_merged/accepted_hits.bam",
         genome = "hg19",
-        location = "chr8:128,747,267-128,754,546",
-        browser = igv.createBrowser(el, {
+        location = "chr8:128,747,267-128,754,546";
+        igv.createBrowser(el, {
           showNavigation: true,
           showRuler: true,
           genome: genome,
           locus: location,
-          tracks: [
-            {
-              name: "Genes",
-              type: "annotation",
-              format: "bed",
-              sourceType: "file",
-              url: bed,
-              indexURL: bedindex,
-              order: Number.MAX_VALUE,
-              visibilityWindow: 300000000,
-              displayMode: "EXPANDED"
-            }, {
-              url: bam,
-              name: 'Brain (BodyMap)'
-            }
-          ]
-        });
+        }),
         
+        igv.browser.loadTrack({
+          name: "Genes",
+          type: "annotation",
+          format: "bed",
+          sourceType: "file",
+          url: bed,
+          indexURL: bedindex,
+          order: Number.MAX_VALUE,
+          visibilityWindow: 300000000,
+          displayMode: "EXPANDED"
+        }),
+        
+        igv.browser.loadTrack({
+          url: bam,
+          name: 'Brain (BodyMap)'
+        });
       },
 
       resize: function(width, height) {
